e7a3e07e3ef97d80a8f37f7fa436bc3cf7da5404
gergomotyovszki <gergo.motyovszki@gmail.com>
2025-11-19 18:45:02 +0000

    Deploying to gh-pages from @ gergomotyovszki/playground-alfolio@643b4db92bf8a92e24f23e7bf12c9c0d2122362c 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -501,42 +501,56 @@ ninja.data = [{
         title: 'CV',
         section: 'Socials',
         handler: () => {
-          window.open("/playground-alfolio/assets/pdf/example_pdf.pdf", "_blank");
+          window.open("/playground-alfolio/assets/pdf/cv_motyo.pdf", "_blank");
         },
       },{
         id: 'social-email',
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
+          window.open("mailto:%67%65%72%67%6F.%6D%6F%74%79%6F%76%73%7A%6B%69@%65%63.%65%75%72%6F%70%61.%65%75", "_blank");
         },
       },{
-        id: 'social-inspire',
-        title: 'Inspire HEP',
+        id: 'social-linkedin',
+        title: 'LinkedIn',
         section: 'Socials',
         handler: () => {
-          window.open("https://inspirehep.net/authors/1010907", "_blank");
-        },
-      },{
-        id: 'social-rss',
-        title: 'RSS Feed',
-        section: 'Socials',
-        handler: () => {
-          window.open("/playground-alfolio/feed.xml", "_blank");
+          window.open("https://www.linkedin.com/in/gergo-motyovszki", "_blank");
         },
       },{
         id: 'social-scholar',
         title: 'Google Scholar',
         section: 'Socials',
         handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
+          window.open("https://scholar.google.com/citations?user=Cq7oeYIAAAAJ", "_blank");
         },
       },{
-        id: 'social-custom_social',
-        title: 'Custom_social',
+        id: 'social-orcid',
+        title: 'ORCID',
         section: 'Socials',
         handler: () => {
-          window.open("https://www.alberteinstein.com/", "_blank");
+          window.open("https://orcid.org/0000-0002-1229-7564", "_blank");
+        },
+      },{
+        id: 'social-github',
+        title: 'GitHub',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://github.com/gergomotyovszki# your GitHub user name", "_blank");
+        },
+      },{
+        id: 'social-bluesky',
+        title: 'Bluesky',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://bsky.app/profile/motyo6.bsky.social", "_blank");
+        },
+      },{
+        id: 'social-x',
+        title: 'X',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://twitter.com/motyo6", "_blank");
         },
       },{
       id: 'light-theme',
